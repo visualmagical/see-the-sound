a1df812fb7ddf36db5dc5c0ec599cd7afbc037d2
Katia Trifonova <trifonova.katia@gmail.com>
2018-01-14 16:46:38 +0300

    voice add

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+///////////////////////////  audio  ///////////////////////////  
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioCtx     = new AudioContext(),
     audioElement,
-    audioSrc,
+    audioSrc     = null,
     analyser     = audioCtx.createAnalyser(),
-    numberOfBars = 180,
+    bufferLength = analyser.frequencyBinCount,
+    frequencyData = new Uint8Array(bufferLength);
+
+analyser.fftSize = 2048;
+analyser.minDecibels = -90;
+analyser.maxDecibels = 0; 
+
+///////////////////////////  visuals  ///////////////////////////  
+var numberOfBars = 180,
     spectrum     = document.getElementById('spectrum'),
     W            = window.innerWidth,
     H            = (window.innerHeight - 60),
     size         = Math.min(W, H),
+
+///////////////////////////  UI   ///////////////////////////
     playBtn      = document.querySelector('.play'),
     bars         = [],
     fileBtn      = document.querySelector('.file-open'),
@@ -16,18 +27,11 @@ var audioCtx     = new AudioContext(),
     mp3          = document.querySelector('.mp3'),
     listItems    = document.querySelectorAll('.item'),
     hello        = document.querySelector('.hello'),
-    recButton    = document.querySelector('.rec'),
     record       = document.querySelector('.record');
 
-analyser.fftSize = 2048;
-analyser.minDecibels = -90;
-analyser.maxDecibels = 0; 
-// analyser.smoothingTimeConstant = 1;
 
 
-var bufferLength = analyser.frequencyBinCount,
-    frequencyData = new Uint8Array(bufferLength);
-
+///////////////////////////  visuals  ///////////////////////////
 
 var MusicVisuals = {
   call: null,
@@ -70,8 +74,6 @@ var MusicVisuals = {
         barcount = 0;
       }
 
-
-      
       let bar = bars[barcount];
       
       if (bar) {
@@ -93,6 +95,68 @@ var MusicVisuals = {
   }
 };
 
+
+
+///////////////////////////  events / function calls  ///////////////////////////
+
+handleList();
+
+fileInput.addEventListener('change', function(e) {
+  e.stopPropagation();
+  
+  var file = fileInput.files[0];
+
+  audioElement = document.createElement('audio');
+  audioElement.src = URL.createObjectURL(file);
+  audioElement.id = 'audioElement';
+  document.body.appendChild(audioElement);
+  audioSrc     = audioCtx.createMediaElementSource(audioElement);
+  audioSrc.connect(audioCtx.destination);
+  audioSrc.connect(analyser);
+
+  playBtn.classList.add('dis-block');
+  upload.classList.remove('dis-flex');
+  audioElement.addEventListener('ended', onEnded);
+
+})
+
+playBtn.addEventListener('click', function(){
+  playSound();
+  MusicVisuals.render();
+  MusicVisuals.start();
+});
+
+
+hello.addEventListener('click', function(e) {
+  e.stopPropagation();
+  if ( this.classList.contains('move') ) {
+    
+    console.log('move detected');
+    for (let j = 0; j < listItems.length; j++) {
+      listItems[j].classList.remove('disabled');
+      listItems[j].classList.remove('collapse');
+    }
+    
+    this.classList.remove('move');
+    record.classList.remove('ap-disap');
+    mp3.classList.remove('dis-block');
+    upload.classList.remove('dis-flex');
+    audioSrc.disconnect(); 
+    audioSrc = null;
+  }
+})
+
+
+
+
+/////////////////////////// functions  ///////////////////////////
+
+function loadMp3() {
+  
+}
+
+
+
 function playSound() {
   if (!audioElement.paused) {
     audioElement.pause();
@@ -107,74 +171,56 @@ function playSound() {
       audioElement.play();
     },300)
   }
+  console.log("audiosrc: " + audioSrc);
 }
-
 
 function onEnded() {
   document.body.removeChild(this);
   upload.classList.add('dis-flex');
   playBtn.classList.remove('dis-block');
   playBtn.style.opacity = "1";
+  audioSrc.disconnect(); 
+  audioSrc = null; 
+}
+
+
+function handleList() {
+  for (let j = 0; j < listItems.length; j++) {
+
+    listItems[j].addEventListener("click", function(e) {
+      console.log('click!');
+      e.stopPropagation();
+       
+      listItems[j].classList.add('button--active');
+      listItems[j].classList.add('disabled');
+
+      removeOtherItems();
+      listItems[j].classList.remove('button--active');
+      hello.classList.add('move');
+      
+      let dataType = this.getAttribute('data-type');
+      
+
+      if (dataType === "sound") {
+        record.classList.remove('ap-disap');
+        mp3.classList.add('dis-block');
+        upload.classList.add('dis-flex');
+
+      } else if ( dataType === "voice" ) {
+        mp3.classList.remove('dis-block');
+        upload.classList.remove('dis-flex');
+
+        startRec();
+        MusicVisuals.render();
+        MusicVisuals.start();
+      } 
+    })
+  }
 }
 
 
 
-fileInput.addEventListener('change', function(e) {
-  e.stopPropagation();
-  var file = this.files[0];
-
-  audioElement = document.createElement('audio');
-  audioElement.src = URL.createObjectURL(file);
-  audioElement.id = 'audioElement';
-  document.body.appendChild(audioElement);
-  audioSrc     = audioCtx.createMediaElementSource(audioElement);
-  audioSrc.connect(audioCtx.destination);
-  audioSrc.connect(analyser);
-
-  playBtn.classList.add('dis-block');
-  upload.classList.remove('dis-flex');
-  audioElement.addEventListener('ended', onEnded);
-})
-
-playBtn.addEventListener("click", playSound);
-recButton.addEventListener("click", function(){
-  startRec();
-  MusicVisuals.render();
-  MusicVisuals.start();
-
-});
-// MusicVisuals.render();
-// MusicVisuals.start();
-
-
-for (let j = 0; j < listItems.length; j++) {
-
-  listItems[j].addEventListener("click", function() {
-     
-    this.classList.add('button--active');
-    removeOther();
-    this.classList.remove('button--active');
-    hello.classList.add('move');
-    
-    let dataType = this.getAttribute('data-type');
-    console.log(dataType);
-
-    if (dataType === "sound") {
-      record.classList.remove('dis-block');
-      recButton.classList.remove('dis-block');
-      mp3.classList.add('dis-block');
-      upload.classList.add('dis-flex');
-    } else if ( dataType === "voice" ) {
-      mp3.classList.remove('dis-block');
-      upload.classList.remove('dis-flex');
-      record.classList.add('dis-block');
-      recButton.classList.add('dis-block');
-    }
-  })
-}
-
-
-function removeOther () {
+function removeOtherItems () {
   for (let k = 0; k < listItems.length; k++ ) {
     if (!listItems[k].classList.contains('button--active')) {
         listItems[k].classList.add('collapse');
@@ -182,27 +228,25 @@ function removeOther () {
   }
 }
 
-
-
- 
 function startRec() {
-
+  if (audioSrc === null) {
     navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
     navigator.getUserMedia(
         { audio: true, video: false }, 
         function (mediaStream) {
             audioSrc = audioCtx.createMediaStreamSource(mediaStream);
             audioSrc.connect(analyser);
-            // analyser.connect(audioCtx.destination);
-            console.log(audioElement);
+            record.classList.add('ap-disap'); 
         }, 
         function (error) {
-            console.log("There was an error when getting microphone input: " + err);
+          console.log("There was an error when getting microphone input: " + err);
         }
     );
-}
- 
-function stopRec() {
+  } else {
     audioSrc.disconnect(); 
     audioSrc = null; 
+  }
 }
+
+
+
